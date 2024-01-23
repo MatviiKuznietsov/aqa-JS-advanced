7ed8d469822959febe7d549d3bd10aa92eff4585
MTV <matveimtvcool@gmai.com>
2024-01-23 09:57:24 +0200

    HW_003 done

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+const userName1 = "Jack"
+const userName2 = "Mike"
+let greetingMsgConcatenation = 'Hello ' + userName1 + " and " + userName2
+let greetingMsgPattern  = `Hello ${userName1} and ${userName2}`
+console.log(greetingMsgConcatenation)
+console.log(greetingMsgPattern)
---
+++ jsBasics/task3.js
@@ -0,0 +1,3 @@
+const age = 17;
+const isAgeAdult = age >= 18
+console.log("The man is adult =", isAgeAdult)

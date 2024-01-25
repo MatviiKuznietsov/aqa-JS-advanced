e28242dcd0e598c31393d977e98bdfdeaf38ba1e
MTV <matveimtvcool@gmai.com>
2024-01-25 21:07:53 +0200

    HW_004 done add details

--- hwAqaJs/hw004/task3.js
+++ hwAqaJs/hw004/task3.js
@@ -4,8 +4,8 @@ for (let i = 1; i <= 10; i++) {
     console.log(`${numberMultiplication}*${i}=${numberMultiplication * i}`)
 }
 console.log('while example')
-let j = 1
-while (j <= 10) {
-    console.log(numberMultiplication + '*' + j + '=' + numberMultiplication * j)
-    j++
+let index = 1
+while (index <= 10) {
+    console.log(numberMultiplication + '*' + index + '=' + numberMultiplication * index)
+    index++
 }

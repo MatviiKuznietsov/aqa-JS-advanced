b712fd01692576b8c20707898c6ffef5bc417fbf
MTV <matveimtvcool@gmai.com>
2024-01-29 21:27:44 +0200

    HW_005 done

---
+++ hwAqaJs/hw005/task2.js
@@ -0,0 +1,8 @@
+function getArea(age) {
+    return age >= 18
+}
+
+const age1 = 15
+const age2 = 25
+console.log(`Man adult with age ${age1} = ${getArea(age1)}`)
+console.log(`Man adult with age ${age2} = ${getArea(age2)}`)

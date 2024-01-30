e880f6611b451f09d6753e70bb424c428e0f7374
MTV <matveimtvcool@gmai.com>
2024-01-30 07:38:30 +0200

    HW_005  refactor function's name

--- hwAqaJs/hw005/task2.js
+++ hwAqaJs/hw005/task2.js
@@ -1,8 +1,8 @@
-function getArea(age) {
+function isAdult(age) {
     return age >= 18
 }
 
 const age1 = 15
 const age2 = 25
-console.log(`Man adult with age ${age1} = ${getArea(age1)}`)
-console.log(`Man adult with age ${age2} = ${getArea(age2)}`)
+console.log(`Man adult with age ${age1} = ${isAdult(age1)}`)
+console.log(`Man adult with age ${age2} = ${isAdult(age2)}`)
